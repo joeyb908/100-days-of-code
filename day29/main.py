31ebdcb50b4ce37147c80c24bcd2128a437899aa
Joey Ohannesian <joeyb908@gmail.com>
2022-04-27 12:48:39 -0400

    Add files via upload
    Finished product.
    
    - password generator
    - custom password of user choice
    - enter website password to be used on
    - autopopulate email with user's email to save user having to type email over and over again
    - copy password to clipboard automatically after generating new password

--- day29/main.py
+++ day29/main.py
@@ -1,9 +1,85 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+
 FONT = ("Cambria", 12)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate_password():
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    [password_list.append(random.choice(letters)) for char in range(nr_letters)]
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+
+    [password_list.append(random.choice(symbols)) for symbol in range(nr_symbols)]
+    # for char in range(nr_symbols):
+    #   password_list += random.choice(symbols)
+
+    [password_list.append(random.choice(numbers)) for number in range(nr_numbers)]
+    # for char in range(nr_numbers):
+    #   password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    # password = ""
+    # for char in password_list:
+    #     password += char
+    password = "".join(password_list)
+    pass_entry.delete(0, END)
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    """Save data to a file, 'data.txt'"""
+
+    # get website, username, and pass input
+    website = website_entry.get()
+    username = username_entry.get()
+    password = pass_entry.get()
+
+
+
+    if (len(website) or len(password)) == 0 or (len(website) and len(password)) == 0:
+        messagebox._show(title="Field Empty", message="You left either the website or password blank!")
+
+    else:
+        is_okay = messagebox.askokcancel(title=website, message=f"These are the details entered:\n"
+                                                      f"Email: {website}\n"
+                                                      f"Username: {username}\n"
+                                                      f"Password: {password}\n"
+                                                      f"Is it okay to save?")
+
+        if is_okay:
+            # open data.txt, if doesn't exist, creates and appends to end
+            with open("data.txt", "a") as data:
+                data.write(f"{website}, {username}, {password}\n")
+
+            # clears website and password fields
+            clear_fields()
+
+
+def clear_fields():
+    """Clears website and password fields"""
+
+    website_entry.delete(0, END)
+    pass_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # create initial window
@@ -31,22 +107,24 @@ password_label.grid(column=0, row=3, sticky="E")
 
 # create website entry and left/right align
 website_entry = Entry(width=35)
+website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
 
 # create username entry and left/right align
 username_entry = Entry(width=35)
+username_entry.insert(0, string="joeyb908@gmail.com")
 username_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
 
 # create passgen entry and left/right align
-pass_gen = Entry(width=21)
-pass_gen.grid(column=1, row=3, sticky="EW")
+pass_entry = Entry(width=21)
+pass_entry.grid(column=1, row=3, sticky="EW")
 
 # create generate button
-generate_pass_button = Button(text="Generate Password", font=FONT)
+generate_pass_button = Button(text="Generate Password", font=FONT, command=generate_password)
 generate_pass_button.grid(column=2, row=3)
 
 # create add button and left/right  align
-add_button = Button(text="Add", font=FONT, width=35)
+add_button = Button(text="Add", font=FONT, width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 
