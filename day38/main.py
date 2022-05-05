aa68c12769c579dffb49dd850e0cc013926b0467
Joey Ohannesian <joeyb908@gmail.com>
2022-05-05 07:09:34 -0400

    Add files via upload

--- day38/main.py
+++ day38/main.py
@@ -1,5 +1,5 @@
 import os
-import json
+import base64
 import requests
 import datetime as dt
 
@@ -12,33 +12,38 @@ headers = {
     "x-remote-user-id": x_remote_user_id
 }
 
-#exercises = input("Tell me which exercises you did?\n")
+# exercises = input("Tell me which exercises you did?\n")
 exercises = "run 5km and walk 2 miles"
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 exercise_parameters ={
     "query": exercises,
 }
 today = dt.datetime.today()
-today_date = today.strftime("%d%m%Y")
+today_date = today.strftime("%d/%m/%Y")
 today_time = today.time().strftime("%H:%M:%S")
 response = requests.post(url=exercise_endpoint, json=exercise_parameters, headers=headers)
 exercise_response = response.json()["exercises"]
+print(exercise_response)
 
-exercise_data = [
-    {
-        "Date": today_date,
-        "Time": today_time,
-        "Exercise": exercise_response[index]["name"].title(),
-        "Duration": exercise_response[index]["duration_min"],
-        "Calories": exercise_response[index]["nf_calories"]
-    } for index in range(0, len(exercise_response))]
-
-exercise_json = json.dumps(exercise_data)
-print(exercise_json)
-
-sheetly_username = os.environ["sheetly_username"]
-sheetly_project_name = "workoutTracking"
+sheetly_username = "35c919a6ec30d59b9847994261f581b9"
+sheetly_project_name = "myWorkouts"
 sheetly_sheet_name = "workouts"
+sheetly_key = "alkwdjalskdjad"
 sheetly_endpoint = f"https://api.sheety.co/{sheetly_username}/{sheetly_project_name}/{sheetly_sheet_name}"
-response = requests.post(url=sheetly_endpoint, json=exercise_json)
-print(response.text)
+headers = {
+    "Authorization": "Bearer poop"
+}
+
+exercise_data = {}
+for index in range(0, len(exercise_response)):
+    exercise_data = {
+        "workout": {
+            "exercise": exercise_response[index]["name"].title(),
+            "date": today_date,
+            "time": today_time,
+            "duration": exercise_response[index]["duration_min"],
+            "calories": exercise_response[index]["nf_calories"]
+            }
+        }
+    response = requests.post(sheetly_endpoint, json=exercise_data, headers=headers)
+    print(response.text)
