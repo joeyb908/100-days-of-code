48b082fca62a63b791d4c4fe35b03b2552e67829
Joey Ohannesian <joeyb908@gmail.com>
2022-06-07 17:48:32 -0400

    Add files via upload
    added some extra comments

--- day48/cookie_clicker_classic.py
+++ day48/cookie_clicker_classic.py
@@ -12,9 +12,13 @@ def click_cookie():
 
 
 def determine_item_costs():
+    """Creates a dictionary that holds all purchasable items and associated costs"""
+
+    # find the store element and create a separate store dictionary to hold everything
     store = browser.find_elements(By.CSS_SELECTOR, '#store b')[:8]
     purchase_cost = {}
 
+    # go through the store, determine the item and the cost of each item
     for n in range(0, len(store)):
         split_text = store[n].text.split('-')
         if split_text == ['']:
@@ -25,25 +29,38 @@ def determine_item_costs():
                 "cost": int(split_text[1].strip().replace(',', ''))
             }
 
+    # return the completed dictionary
     return purchase_cost
 
 
 def make_purchase():
+    """Make a purchase of an item from most expensive to cheapest"""
+
+    # for each item in the dictionary, if the total cookies owned is greater than the item, make the purchase
     for x in range(7, 0, -1):
         total_cookies = browser.find_element(By.ID, 'money').text
+
+        # commas mess up integers (not technically base 10)
         if ',' in total_cookies:
             total_cookies = total_cookies.replace(',', '')
             total_cookies = int(total_cookies)
+
+        # no comma, no problem
         else:
             total_cookies = int(total_cookies)
+
+        # if the total cookies is more than the specific item, purchase it
         if total_cookies > store_dict[x]['cost']:
             try:
                 browser.find_element(By.ID, f'buy{store_dict[x]["item"]}').click()
+
+            # once an item goes from being able to be purchased to unpurchasable, it causes some wonky issues
             except selenium.common.exceptions.StaleElementReferenceException:
                 pass
 
 
 def reset_purchase_timeout():
+    """Reset the purchase timeout"""
     return time.time() + 5
 
 
